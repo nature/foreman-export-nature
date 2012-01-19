e1d14282b87da8e1593a22baf10b76071eb9a977
Chris Lowder <clowder@gmail.com>
2012-01-19 17:17:48 +0000

    Setup runit style service logging.

--- lib/foreman/export/nature_runit/service.rb
+++ lib/foreman/export/nature_runit/service.rb
@@ -1,15 +1,18 @@
 class Foreman::Export::NatureRunit::Service
   @run_template = Foreman::Export::NatureRunit.template_root.join('run.erb')
+  @log_template = Foreman::Export::NatureRunit.template_root.join('log.erb')
 
   class << self
-    attr_reader :run_template
+    attr_reader :run_template, :log_template
   end
 
-  attr_reader :command, :target, :execution_target, :active_target, :environment, :environment_target
+  attr_reader :command, :target, :execution_target, :active_target, :environment,
+    :environment_target, :logging_target
 
   def initialize(name, command, execution_target, export_target, environment)
     @target             = export_target.join(name).expand_path
     @active_target      = export_target.join('..', '..', 'service', name).expand_path
+    @logging_target     = export_target.join('..', '..', 'var', 'log', name).expand_path
     @environment_target = @target.join('env').expand_path
     @environment        = environment
     @execution_target   = execution_target
@@ -17,6 +20,26 @@ class Foreman::Export::NatureRunit::Service
   end
 
   def create!
+    export_run_script!
+    export_log_script!
+  end
+
+  def export_log_script!
+    log_script_path = target.join('log', 'run')
+
+    create_if_missing(target.join('log'))
+    create_if_missing(logging_target)
+
+    write_file(log_script_path, log_script)
+    FileUtils.chmod(0755, log_script_path.to_s)
+  end
+
+  def log_script
+    erb_template = ERB.new(self.class.log_template.read)
+    erb_template.result(binding)
+  end
+
+  def export_run_script!
     run_script_path = target.join('run')
 
     create_if_missing(target)
